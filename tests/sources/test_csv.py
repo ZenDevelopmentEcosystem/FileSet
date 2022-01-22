d1bc79ee3ef35814f7c163b196ca6f0c2cdee4d8
Per Böhlin <per.bohlin@devconsoft.se>
2022-01-22 15:54:20 +0100

    Convert to fixtures in csv unit-tests

--- tests/sources/test_csv.py
+++ tests/sources/test_csv.py
@@ -3,7 +3,7 @@ import pytest
 from fileset.exceptions import FileSetException
 from fileset.sources.csv import CsvSource, constructor, representer
 
-
+@pytest.fixture(name='raw')
 def get_raw():
     return {
         'index-file': 'index.csv',
@@ -13,35 +13,32 @@ def get_raw():
         'root-path': '/mnt/data'
     }
 
-
+@pytest.fixture(name='csv')
 def get_csv():
     return CsvSource('index.csv', 'id', 'filename', '.txt', '/mnt/data')
 
 
-def test_constructor():
-    result = constructor(get_raw())
-    expected = get_csv()
+def test_constructor(raw, csv):
+    result = constructor(raw)
     assert isinstance(result, CsvSource)
-    assert result == expected
+    assert result == csv
 
 
-def test_constructor_missing_property_raise_exception():
-    raw = get_raw()
+def test_constructor_missing_property_raise_exception(raw):
     del raw['index-file']
     with pytest.raises(FileSetException) as ctx:
         constructor(raw)
     assert ctx.value.args[0] == "Missing property 'index-file' in csv definition"
 
 
-def test_constructor_invalid_property_raise_exception():
-    raw = get_raw()
+def test_constructor_invalid_property_raise_exception(raw):
     raw['INVALID'] = ''
     with pytest.raises(FileSetException) as ctx:
         constructor(raw)
     assert ctx.value.args[0] == "Invalid property 'INVALID' in csv definition"
 
 
-def test_representer():
-    result = representer(get_csv())
-    expected = {'csv': get_raw()}
+def test_representer(csv, raw):
+    result = representer(csv)
+    expected = {'csv': raw}
     assert result == expected
